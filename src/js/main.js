333f56c4a73c8c878ad45e5dc51312715a703378
Hiroshi Nagata <HIROSN@LIVE.COM>
2018-01-13 13:55:07 -0800

    Move the inject_stylesheet code to app.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-require("./lib/social");
+(function() {
+  var href = 'css.css';
+  var head = document.getElementsByTagName('head')[0];
+  if (head.querySelector('link[href="' + href + '"]')) {
+    return;
+  }
+  var link = document.createElement('link');
+  link.setAttribute('rel', 'stylesheet');
+  link.setAttribute('href', href);
+  head.appendChild(link);
+})();
